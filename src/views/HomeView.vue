499bded57f7bba0ac11f7df1c896afba5748e414
nospame <mapson.evan@gmail.com>
2022-04-07 11:52:21 -0700

    add fruit create and show actions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,9 @@ export default {
   data: function () {
     return {
       message: "This is the Fruits App",
-      fruits: []
+      fruits: [],
+      currentFruit: {},
+      newFruit: {}
     };
   },
   created: function () {
@@ -18,6 +20,20 @@ export default {
           console.log(response.data);
           this.fruits = response.data;
         })
+    },
+    showFruit: function (fruit) {
+      console.log('in fruit show');
+      this.currentFruit = fruit;
+      console.log(this.currentFruit);
+      document.querySelector("#fruit-details").showModal();
+    },
+    createFruit: function (fruit) {
+      console.log('in fruit create');
+      axios.post('/fruits', fruit)
+        .then(response => {
+          console.log(response.data);
+          this.fruits.push(response.data);
+        })
     }
   },
 };
@@ -26,8 +42,48 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>
+      Name:
+      <input v-model="newFruit.name" />
+    </p>
+    <p>
+      Color:
+      <input v-model="newFruit.color" />
+    </p>
+    <p>
+      Family:
+      <input v-model="newFruit.family" />
+    </p>
+    <p>
+      Image URL:
+      <input v-model="newFruit.image_url" />
+    </p>
+    <button v-on:click="createFruit(newFruit)">Add Fruit</button>
+    <div v-for="fruit in fruits">
+      <img v-bind:src="fruit.image_url" class="index-img" />
+      <br />
+      {{ fruit.name }}
+      <br />
+      <button v-on:click="showFruit(fruit)">Details</button>
+    </div>
+    <dialog id="fruit-details">
+      <form method="dialog">
+        <img v-bind:src="currentFruit.image_url" class="show-img" />
+        <p>Name: {{ currentFruit.name }}</p>
+        <p>Family: {{ currentFruit.family }}</p>
+        <p>Color: {{ currentFruit.color }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
-  <div v-for="fruit in fruits">{{ fruit.name }}</div>
 </template>
 
-<style></style>
+<style>
+.index-img {
+  width: 200px;
+}
+
+.show-img {
+  width: 450px;
+}
+</style>

5657fcaf2769d1eb323b159096be1b1b86a8e552
nospame <mapson.evan@gmail.com>
2022-04-07 11:15:13 -0700

    add fruits index action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
+<script>
+import axios from 'axios'
+export default {
+  data: function () {
+    return {
+      message: "This is the Fruits App",
+      fruits: []
+    };
+  },
+  created: function () {
+    this.indexFruits();
+  },
+  methods: {
+    indexFruits: function () {
+      console.log('in fruits index');
+      axios.get('/fruits')
+        .then(response => {
+          console.log(response.data);
+          this.fruits = response.data;
+        })
+    }
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
+  <div v-for="fruit in fruits">{{ fruit.name }}</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style></style>
